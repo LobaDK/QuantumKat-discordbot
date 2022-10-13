0df10d43b8c7bc17f4a5860d6176df03aa81c474
Nicklas <nicklashelsberg@gmail.com>
2022-10-13 08:12:07 +0200

    More code

--- cogs/RegexCommands.py
+++ cogs/RegexCommands.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
+import re
 
 class RegexCommands(commands.cog):
     def __init__(self, bot):
         self.bot = bot
 
     async def regexChecks(self, message):
+        pettRegex = re.compile(r"^!(p+e+t+(s*))", re.IGNORECASE)
         com = message.content.split(' ', 1)
 
         if len(com) > 1:
@@ -14,4 +16,11 @@ class RegexCommands(commands.cog):
             dest = message.author.display_name
             destLC = ''
 
-        if message.content
+        test = pettRegex.match(com[0])
+        if test:
+            pett, plural = test.groups()
+            if plural == '':
+                plural = 's'
+            else: plural = ''
+
+            
